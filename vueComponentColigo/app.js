c7adc146353bc2f21fafc66dca8daea14ade4557
luis <yongchun1683@gmail.com>
2018-06-05 21:20:52 +0800

    add vue component from coligo

---
+++ vueComponentColigo/app.js
@@ -0,0 +1,15 @@
+// simple component
+Vue.component('greeting-component', {
+    template:
+        `
+            <h1>Welcome to Vue Component</h1>
+        `
+})
+
+
+new Vue({
+    el: '#app',
+    data: {
+
+    }
+})
